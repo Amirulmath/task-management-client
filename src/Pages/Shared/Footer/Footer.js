a77115dd26209d16cbf72862d4a77b3a4741175f
Amirulmath <amirulmath760@gmail.com>
2022-12-29 01:24:41 +0600

    Added Footer Section

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p>Footer</p>
+            <footer className="p-4 bg-dark text-white text-center">
+                <b>Copyright @ 2022 Programming-hero team </b>
+            </footer>
         </div>
     );
 };
